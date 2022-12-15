920050ca87d83fcfea99d051cdaf74286933b828
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-12-15 23:04:08 +0300

    Rendering driver ranks on the website directly from the API on click event

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-// async function getDriverRanks() {
-//     try {
-//         const resp = await window.fetch('https://api-formula-1.p.rapidapi.com/rankings/drivers?season=2021', {method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '4818705ef7msh949a70a405c962fp1fea89jsn66dada7a2c3b',
-//             'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
-//         }})
-//         const data = await resp.json();
-//         console.log(data);
+async function getDriverRanksByYear(year) {
+    try {
+        const resp = await window.fetch(`https://api-formula-1.p.rapidapi.com/rankings/drivers?season=${year}`, {method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4818705ef7msh949a70a405c962fp1fea89jsn66dada7a2c3b',
+            'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+        }})
+        const data = await resp.json();
+        console.log(data);
 
-//         // return data => data.forEach(item => showDrivers(item));
-//     } catch (err) {
-//         console.log(err);
+        return data.response.forEach(items => renderDriverSeasonRanks(items));
+    } catch (err) {
+        console.log(err);
 
-//         return [];
-//     }
-// }
+        return [];
+    }
+}
 
 // function init() {
 //     getDriverRanks();
@@ -30,53 +30,62 @@
 //     .then(data => console.log(data))
 // }
 
-async function getDriversFromServer() {
-    try {
-        const resp = await window.fetch('http://localhost:3000/response', { method: "GET" });
-        const data = await resp.json();
+// async function getDriversFromServer() {
+//     try {
+//         const resp = await window.fetch('http://localhost:3000/response', { method: "GET" });
+//         const data = await resp.json();
 
-        console.log(data)
+//         console.log(data)
 
-        return data;
-    } catch (error) {
-        console.log(error);
+//         return data;
+//     } catch (error) {
+//         console.log(error);
 
-        return [];
-    }
+//         return [];
+//     }
+// }
+
+// function to display the drivers
+function renderDriverSeasonRanks(dereva) {
+    let driverList = document.querySelector('#drivers')
+    let oneDriver = document.createElement('li')
+    oneDriver.className = "pt-6 md:p-8 text-center md:text-left space-y-4" 
+    oneDriver.className = "max-w-sm rounded overflow-hidden shadow-lg"
+    
+
+    oneDriver.innerHTML=`<img src="${dereva.driver.image}" class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" width="284" height="412" />
+        <p class = "text-lg font-medium"> ${dereva.driver.name}, ${dereva.driver.number}</p>
+        <figcaption class="font-medium">
+        <div class="text-red-600 dark:text-red-600">
+            ${dereva.team.name}
+            <div class="flex items-center">
+                <img class="w-10 h-10 rounded-full mr-4" src="${dereva.team.logo}"/>
+            </div>
+        </div>
+        <div class="text-slate-700 dark:text-slate-500">
+            ${dereva.points}, Position: ${dereva.position}
+        </div>
+        </figcaption>
+    `
+    
+    driverList.appendChild(oneDriver)
+        
+
+    
+
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-    const rankings = await getDriversFromServer()
+    const year = parseInt(document.getElementById("years").value)
+    
+    
 
-    // displaying driver names in a list
-    let driverList = document.querySelector('#drivers')
-
-    rankings.forEach((dereva) => {
-        
-        let oneDriver = document.createElement('li')
-        oneDriver.className = "pt-6 md:p-8 text-center md:text-left space-y-4" 
-        oneDriver.className = "max-w-sm rounded overflow-hidden shadow-lg"
-        
-
-        oneDriver.innerHTML=`<img src="${dereva.driver.image}" class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" width="284" height="412" />
-            <p class = "text-lg font-medium"> ${dereva.driver.name}, ${dereva.driver.number}</p>
-            <figcaption class="font-medium">
-            <div class="text-red-600 dark:text-red-600">
-                ${dereva.team.name}
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full mr-4" src="${dereva.team.logo}"/>
-                </div>
-            </div>
-            <div class="text-slate-700 dark:text-slate-500">
-                ${dereva.points}, Position: ${dereva.position}
-            </div>
-            </figcaption>
-        `
-        
-        driverList.appendChild(oneDriver)
-        
+    document.getElementById("years").addEventListener("click", async () => {
+        await getDriverRanksByYear(year)
+        console.log(year)
 
     })
+
 })
     
   
