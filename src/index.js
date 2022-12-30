97216494b9352e923163a511c81d90dbd4b090cf
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-12-30 13:50:09 +0300

    Styling of F1 Champion Button. Added js functionality to it(modal).

--- src/index.js
+++ src/index.js
@@ -214,12 +214,46 @@ function renderAllDrivers(pilot) {
     driversPit.appendChild(oneDriver)
 }
 
+// function to display the modal popup on mouseenter event
+function champModal() {
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    const openModalBtn = document.querySelector(".btn-open");
+    const closeModalBtn = document.querySelector(".btn-close");
+
+    // close modal function
+    const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    };
+
+    // close the modal when the close button and overlay is clicked
+    closeModalBtn.addEventListener("click", closeModal);
+    overlay.addEventListener("click", closeModal);
+
+    // close modal when the Esc key is pressed
+    document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+    });
+
+    // open modal function
+    const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    };
+    // open modal event
+    openModalBtn.addEventListener("mouseenter", openModal);
+}
+
 // function to hold all functions
 function init() {
     driverByYearEvent();
     teamByYearEvent();
     d2s();
     getAllDrivers();
+    champModal();
 }
 
 window.addEventListener("DOMContentLoaded", init)
